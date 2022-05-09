89f83d24db28182661c0ef1164943402bcfe7d69
season <season_1818@163.com>
2022-05-09 18:03:19 +0800

    reply path

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import React, { Suspense } from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 const Home = React.lazy(() => import('./pages/home'));
+const Replay = React.lazy(() => import('./pages/replay'))
 
 
 const Loading = <div>loading...</div>
@@ -16,7 +17,16 @@ function AppRouter() {
           <Home />
         </Suspense>
       </Route>
+      {/* 回放 */}
+      <Route exact path="replay">
+        <Suspense fallback={Loading}>
+          <Replay />
+        </Suspense>
+      </Route>
     </Switch>
+
+    
+
   )
 }
 
